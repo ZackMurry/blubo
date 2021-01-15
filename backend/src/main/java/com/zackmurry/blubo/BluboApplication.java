048937ae0b79a28e226ece95a5c4d92abdfc5a7a
Zack Murry <zackk.murryy@gmail.com>
2021-01-15 15:59:28 -0600

    Run init for frontend and backend

---
+++ backend/src/main/java/com/zackmurry/blubo/BluboApplication.java
@@ -0,0 +1,13 @@
+package com.zackmurry.blubo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluboApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/zackmurry/blubo/BluboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zackmurry.blubo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
