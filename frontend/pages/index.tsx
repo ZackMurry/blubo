99fbdae93c2aca64c82bae1aed191db374b4d232
Zack Murry <zackk.murryy@gmail.com>
2021-01-15 18:47:39 -0600

    Add logo

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Logo from '../components/Logo'
 
 const LandingPage: React.FC = () => {
   return (
     <div>
-      Hello, world!
+      <Logo size='small' />
     </div>
   )
 }
